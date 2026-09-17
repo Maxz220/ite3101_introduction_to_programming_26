8cece8ffeaea1446b3aa2c141608ac4a7b638af4
Maxz220 <maxsupkiller@gmail.com>
2026-09-17 07:45:44 +0000

    [skip actions] lab/lab01/ch01_t10_two_types_of_division.py

--- lab/lab01/ch01_t10_two_types_of_division.py
+++ lab/lab01/ch01_t10_two_types_of_division.py
@@ -2,6 +2,6 @@ cucumbers = 100
 num_people = 6
 
 whole_cucmbers_per_person = cucumbers // num_people
-print(whole_cucmbers_per_person)
+print(whole_cucumbers_per_person)
 
 float_cucmbers_per_person = cucumbers
